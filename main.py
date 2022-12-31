b8296fec3652505f7828ba5aa728fae5584c9494
PPSH <glock878@gmail.com>
2022-12-31 15:48:29 +0200

    Added CSV output for all missed states

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ IMAGE = "blank_states_img.gif"
 state_data_csv = "50_states.csv"
 states_data = pandas.read_csv(state_data_csv)
 states = states_data.state.to_list()
-print(states)
 screen = Screen()
 screen.title("U.S. States Game")
 screen.addshape(IMAGE)
@@ -14,12 +13,17 @@ tom.penup()
 tim = Turtle()
 tim.hideturtle()
 tim.penup()
-guessed_states = 0
-while guessed_states < 50:
-    user_guess = screen.textinput(title=f"{guessed_states}/50 Correct States", prompt="Please guess a state name:").title()
+guessed_states = []
+while len(guessed_states) < 50:
+    user_guess = screen.textinput(title=f"{len(guessed_states)}/50 Correct States", prompt="Please guess a state name:").title()
+    if user_guess == "Exit":
+        missed_states = list(set(states).symmetric_difference(set(guessed_states)))
+        missed_states_table = pandas.DataFrame(missed_states)
+        missed_states_table.to_csv("missed_states.csv")
+        break
     if user_guess in states:
         guessed_state = states_data[states_data.state == user_guess]
         tim.goto(x=int(guessed_state.x), y=int(guessed_state.y))
         tim.write(user_guess)
-        guessed_states += 1
+        guessed_states.append(user_guess)
 screen.exitonclick()
